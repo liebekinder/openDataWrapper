ed7e736a0b12d9f13152a49f38b8e002f4588ee1
liebekinder <sebast_44@hotmail.fr>
2013-06-05 17:38:58 +0200

    test maven

---
+++ src/opendatawrapper/Principale.java
@@ -0,0 +1,14 @@
+package opendatawrapper;
+
+public class Principale {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello, world!");
+	}
+
+}
